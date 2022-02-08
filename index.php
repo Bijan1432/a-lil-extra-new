f47752f0bb1553c672e8658e58b1aca9126fb552
Bijan1432 <91670516+Bijan1432@users.noreply.github.com>
2022-02-08 20:11:54 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,346 @@
+<?php include ('include/header.php');?>
+
+
+
+
+  <section class="banner">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-9">
+          <p>makeup artist</p>
+          <h2>Xenia Popkova</h2>
+        </div>
+        <div class="col-md-3">
+          <div class="box-icon">
+            <a>
+              <img src="img/5ca399312b50681f8ec70113_icon-instgram.svg" alt="" />
+              <p>Write me</p>
+            </a>
+            <a href="">
+              <img src="img/5ca4cf8c6fd13e14b7db4e86_ico-facebook_messenger.svg" alt="" />
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-12">
+          <div class="owl-carousel owl-theme" id="owl-carousel1">
+            <div class="item slide">
+              <img src="img/5cec11015abdf6394455d09c_slide_1@2x-p-2000.jpeg" class="img-fluid" alt="" />
+            </div>
+            <div class="item slide">
+              <img src="img/5cec11015abdf6394455d09c_slide_1@2x-p-2000.jpeg" class="img-fluid" alt="" />
+            </div>
+            <div class="item slide">
+              <img src="img/5cec11015abdf6394455d09c_slide_1@2x-p-2000.jpeg" class="img-fluid" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container about-txt">
+      <div class="row">
+        <div class="col-md-12">
+          <h3>
+            I am a professional makeup artist, bridal stylist and master brow
+            artist. I create bridal makeup and hairstyles for brides, work on
+            commercial photo and video shoots, teach makeup courses for
+            myself, and shape eyebrows with permanent dyes. I receive clients
+            in my cozy studio in Lisbon.
+          </h3>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="service-txt">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          <div class="txt-col">
+            <h2>Services</h2>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-5">
+          <a href="#">
+            <h3>Photo & Video makeup</h3>
+          </a>
+          <a href="#">
+            <h3>Evening look</h3>
+          </a>
+          <a href="#">
+            <h3>wedding image</h3>
+          </a>
+        </div>
+        <div class="col-md-7">
+          <a href="#">
+            <h3>Eyebrow shaping</h3>
+          </a>
+          <a href="#">
+            <h3>Course "Yourself Makeup Artist"</h3>
+          </a>
+          <a href="#">
+            <h3>space shopping</h3>
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="portfolio">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="row">
+            <div class="col-md-4">
+              <div class="txt-col">
+                <h2>Portfolio</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-5 offset-md-7">
+          <div class="gal1 owl-carousel owl-theme owl-carousel2">
+            <img class="img-fluid item" src="img/5cec15de38469c2a0daeef12_mainPage_portfolio_block_3-p-500.jpeg"
+              alt="" />
+            <img class="img-fluid item" src="img/5cec15de38469c2a0daeef12_mainPage_portfolio_block_3-p-500.jpeg"
+              alt="" />
+            <img class="img-fluid item" src="img/5cec15de38469c2a0daeef12_mainPage_portfolio_block_3-p-500.jpeg"
+              alt="" />
+          </div>
+        </div>
+        <div class="col-md-8 mx-auto">
+          <div class="img-middle" style="position: relative">
+            <div class="gal2">
+              <div class="owl-carousel owl-theme owl-carousel2">
+                <div class="item">
+                  <img class="img-fluid" src="img/5cec15de5d0f47d4fca2460b_mainPage_portfolio_block_2-p-800.jpeg"
+                    alt="" />
+                </div>
+                <div class="item">
+                  <img class="img-fluid" src="img/5cec15de5d0f47d4fca2460b_mainPage_portfolio_block_2-p-800.jpeg"
+                    alt="" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-5">
+          <div class="gal3 owl-carousel owl-theme owl-carousel2">
+            <img class="img-fluid item" src="img/5cec15dec0831c66796fe322_mainPage_portfolio_block_1-p-500.jpeg"
+              alt="" />
+            <img class="img-fluid item" src="img/5cec15dec0831c66796fe322_mainPage_portfolio_block_1-p-500.jpeg"
+              alt="" />
+            <img class="img-fluid item" src="img/5cec15dec0831c66796fe322_mainPage_portfolio_block_1-p-500.jpeg"
+              alt="" />
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12">
+            <a class="btn big-btn" href="">
+              <h3>Contact Us</h3>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="reviews">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          <div class="txt-col">
+            <h2>Reviews</h2>
+          </div>
+        </div>
+      </div>
+      <div class="review-box">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="row">
+              <div class="col-md-3">
+                <div class="front">
+                  <div class="face">
+                    <img class="img-fluid" src="img/5ce293d80b5f0b98b3270fb5_viktoria.lisbon.jpg" alt="" />
+                    <h4>Emma Watson</h4>
+                    <p>New York</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-5 offset-md-4">
+                <div class="testi">
+                  <div class="txt">
+                    <p>
+                      Thank you for your professional and very cordial attitude.
+                      Being your model is a huge gift! You are funny and
+                    </p>
+                    <p class="collapse multi-collapse" id="multiCollapseExample2">
+                      talented. I can't wait for my next makeup. ❤️❤️❤️
+                    </p>
+
+                    <button class="" type="button" data-bs-toggle="collapse" data-bs-target="#multiCollapseExample2"
+                      aria-expanded="false" aria-controls="multiCollapseExample2"><i
+                        class="las la-angle-down"></i></button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md12">
+
+            <div class="border2">
+
+            </div>
+
+          </div>
+        </div>
+      </div>
+      <div class="review-box">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="row">
+              <div class="col-md-3">
+                <div class="front">
+                  <div class="face">
+                    <img class="img-fluid" src="img/5ce293d80b5f0b98b3270fb5_viktoria.lisbon.jpg" alt="" />
+                    <h4>Emma Watson</h4>
+                    <p>New York</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-5 offset-md-4">
+                <div class="testi">
+                  <div class="txt">
+                    <p>
+                      Thank you for your professional and very cordial attitude.
+                      Being your model is a huge gift! You are funny and
+                    </p>
+                    <p class="collapse multi-collapse" id="multiCollapseExample3">
+                      talented. I can't wait for my next makeup. ❤️❤️❤️
+                    </p>
+
+                    <button class="" type="button" data-bs-toggle="collapse" data-bs-target="#multiCollapseExample3"
+                      aria-expanded="false" aria-controls="multiCollapseExample3"><i
+                        class="las la-angle-down"></i></button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md12">
+
+            <div class="border2">
+
+            </div>
+
+          </div>
+        </div>
+      </div>
+      <!-- <div class="review-box">
+  <div class="row">
+          <div class="col-md-12">
+            <div class="row">
+              <div class="col-md-3">
+                <div class="front">
+                  <div class="face">
+                    <img
+                      class="img-fluid"
+                      src="img/5ce293d80b5f0b98b3270fb5_viktoria.lisbon.jpg"
+                      alt=""
+                    />
+                    <h4>Emma Watson</h4>
+                    <p>New York</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-6 offset-md-3">
+                <div class="testi">
+                  <div class="txt">
+                  
+                    <div class="accordion" id="accordionExample">
+                      <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingThree">
+                          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                            <p>
+                              Thank you for your professional and very cordial attitude.
+                              Being your model is a huge gift! You are funny and
+                            </p>
+                          </button>
+                        </h2>
+                        <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                          <div class="accordion-body">
+                            <p>                      talented. I can't wait for my next makeup. ❤️❤️❤️
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+
+                    </div>
+
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+</div> -->
+
+      <div class="review-box">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="row">
+              <div class="col-md-3">
+                <div class="front">
+                  <div class="face">
+                    <img class="img-fluid" src="img/5ce293d80b5f0b98b3270fb5_viktoria.lisbon.jpg" alt="" />
+                    <h4>Emma Watson</h4>
+                    <p>New York</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-5 offset-md-4">
+                <div class="testi">
+                  <div class="txt">
+                    <p>
+                      Thank you for your professional and very cordial attitude.
+                      Being your model is a huge gift! You are funny and
+                    </p>
+                    <p class="collapse multi-collapse" id="multiCollapseExample4">
+                      talented. I can't wait for my next makeup. ❤️❤️❤️
+                    </p>
+
+                    <button class="" type="button" data-bs-toggle="collapse" data-bs-target="#multiCollapseExample4"
+                      aria-expanded="false" aria-controls="multiCollapseExample4"><i
+                        class="las la-angle-down"></i></button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+
+
+
+
+
+<?php include ('include/footer.php');?>
---
+++ services.php
@@ -0,0 +1,183 @@
+<?php include('include/header.php'); ?>
+
+
+
+<section class="icon-box">
+
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-4">
+                <div class="bann-box">
+                    <h1>Services</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+</section>
+
+
+
+
+<!-- =================category card===================== -->
+
+
+
+<section class="services">
+    <div class="service">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-10">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="serv-img">
+                                <img src="img/serv1.jpg" alt="">
+                            </div>
+                        </div>
+                        <div class="col-md-8">
+                            <div class="serv-txt">
+                                <h3>
+                                    Wedding Images
+                                </h3>
+
+                                <h4>
+                                    Wedding make-up and hair style
+                                </h4>
+
+
+                                <a href="#">
+
+                                    <div class="btn-box">
+                                        <p>
+                                            More
+                                        </p>
+
+                                        <i class="las la-arrow-right"></i>
+
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+
+
+
+
+                    </div>
+
+
+
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="service">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-10">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="serv-img">
+                                <img src="img/serv1.jpg" alt="">
+                            </div>
+                        </div>
+                        <div class="col-md-8">
+                            <div class="serv-txt">
+                                <h3>
+                                    Wedding Images
+                                </h3>
+
+                                <h4>
+                                    Wedding make-up and hair style
+                                </h4>
+
+
+                                <a href="#">
+
+                                    <div class="btn-box">
+                                        <p>
+                                            More
+                                        </p>
+
+                                        <i class="las la-arrow-right"></i>
+
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+
+
+
+
+                    </div>
+
+
+
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="service">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-10">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="serv-img">
+                                <img src="img/serv1.jpg" alt="">
+                            </div>
+                        </div>
+                        <div class="col-md-8">
+                            <div class="serv-txt">
+                                <h3>
+                                    Wedding Images
+                                </h3>
+
+                                <h4>
+                                    Wedding make-up and hair style
+                                </h4>
+
+
+                                <a href="#">
+
+                                    <div class="btn-box">
+                                        <p>
+                                            More
+                                        </p>
+
+                                        <i class="las la-arrow-right"></i>
+
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+
+
+
+
+                    </div>
+
+
+
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</section>
+
+
+
+
+
+<?php include('include/footer.php'); ?>
