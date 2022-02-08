3c64fe933e027f9ddfbd2f238acf277077277b29
Bijan1432 <91670516+Bijan1432@users.noreply.github.com>
2022-02-08 20:10:21 +0700

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,85 @@
+
+
+  <section class="footer">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-md-2 ms-4">
+          <img src="img/5cb4a420eff7766c16e0e0ae_Muax Logo.svg" alt="">
+        </div>
+
+      </div>
+      <div class="row justify-content-center">
+
+        <div class="col-md-5">
+          <div class="footer-links">
+            <div class="row text-center ">
+
+              <div class="col-md-4">
+                <a href="#">Services</a>
+              </div>
+              <div class="col-md-4">
+                <a href="#">Portfolio</a>
+              </div>
+              <div class="col-md-4">
+                <a href="#">About me</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-md-2 ">
+          <div class="row">
+            <div class="col-md-4 offset-md-1">
+              <div class="img-box"><img src="img/5ca399312b50681f8ec70113_icon-instgram.svg" alt=""></div>
+            </div>
+            <div class="col-md-7">
+              <div class="txt ">
+                <h4>Instagram</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-1 custom-width">/</div>
+        <div class="col-md-2">
+
+          <div class="row">
+            <div class="col-md-4">
+              <div class="img-box"><img src="img/facebook.svg" alt=""></div>
+            </div>
+            <div class="col-md-8">
+              <div class="txt">
+                <h4>Facebook</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-md-6 text-center mt-4 add">
+          <p> 
+            Rua Passeio do Neptuno, 21B, Parque das Nações <br>
++351 913-084-613
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+
+
+
+
+
+
+
+
+
+  <script src="js/bootstrap.bundle.js"></script>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+  <script src="js/custom.js"></script>
+</body>
+
+</html>
---
+++ include/header.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>A Lil Extra</title>
+  <!-- =================fonts============== -->
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Cormorant:wght@300;400;500;600;700&display=swap"
+    rel="stylesheet" />
+  <!-- =================Line awsome Icon================ -->
+  <link rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/line-awesome/1.3.0/line-awesome/css/line-awesome.min.css" />
+
+  <!-- =================bootstrap css============== -->
+
+  <link rel="stylesheet" href="css/bootstrap.min.css" />
+
+  <!-- =================custom css============== -->
+
+  <link rel="stylesheet" href="css/custom.css" />
+
+  <!-- ==================================owl css===================== -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+  <!-- <link rel="stylesheet" href=""> -->
+</head>
+
+<body>
+  <section class="nav">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 p-md-0">
+          <nav class="navbar navbar-expand-lg navbar-light">
+            <div class="container-fluid">
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+                <ul class="navbar-nav mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                  </li>
+                </ul>
+                <a class="navbar-brand" class="d-inline-flex" href="#">
+                  <img src="img/5cb4a420eff7766c16e0e0ae_Muax Logo.svg" alt="" />
+                  <h5>Makeup, Hairstyles, Training</h5>
+                </a>
+                <ul class="navbar-nav mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <secton class="banner-up">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <img src="img/5cb4a4a9bba38c33b410e118_MUAX_BG_LOGO_Inside.svg" alt="" class="img-fluid mx-auto" />
+        </div>
+      </div>
+    </div>
+  </secton>
